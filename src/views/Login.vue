944d886b987136ae79a2906280874ed7b979547b
JavierHdzS <javier24viper@gmail.com>
2020-02-26 19:01:51 -0600

    inicio y registro
    vista de inicio y registro

---
+++ src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="login">
+  <h3>Registrarse</h3>
+  <input type="text" placeholder="Email"><br>
+  <input type="Password" placeholder="Password"><br>
+  <button>Conexión</button>
+  <p>Crear una cuenta</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+        data(){
+            return{};
+        },
+        methods: {}
+    }
+</script>
+<style scoped>
+    .login{
+        margin-top:40px;
+    }
+    input{
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button{
+        margin-top: 20px;
+        width: 10%;
+        cursor: pointer;
+    }
+    p {
+        margin-top: 40px;
+        font-size: 13px;
+    }
+    p a {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="sign-up">
+  <h3>Crear una nueva cuenta</h3>
+  <input type="text" placeholder="Email"><br>
+  <input type="Password" placeholder="Password"><br>
+  <button>Regístrate</button>
+  <span>o volver a iniciar sesión</span>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'signUp',
+        data(){
+            return{};
+        },
+        methods: {}
+    }
+</script>
+<style scoped>
+    .signUp{
+        margin-top:40px;
+    }
+    input{
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button{
+        margin-top: 10px;
+        width: 10%;
+        cursor: pointer;
+    }
+    span {
+        display: block;
+        margin-top: 20px;
+        font-size: 11px;
+    }
+</style>
